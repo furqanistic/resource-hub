88abe41e7cb1b68feff450cdcfdbde0221e9cb96
Furqan <furqan@Furqans-MacBook-Air.local>
2026-02-27 06:03:55 +0500

    fix

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === 'production'
-        ? process.env.FRONTEND_URL || 'https://choice-furqan.netlify.app/'
+        ? process.env.FRONTEND_URL || 'http://187.77.218.189/'
         : ['http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
